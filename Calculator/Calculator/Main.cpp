7442e4114fb29f0ff31087364f60861372823417
Jack Zickrick <zickrickj9207@my.uwstout.edu>
2022-04-22 14:55:34 -0500

    Added basic User input loop

--- Calculator/Calculator/Main.cpp
+++ Calculator/Calculator/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Validate.h"
+#include "Evaluate.h"
 
 using namespace std;
 
@@ -11,7 +12,18 @@ int main() {
 	cout << "Enter an arithmetic expression: ";
 	getline(cin, userInput);
 
+	while (!validate(userIntput) || userInput == "Q" || userInput =="q" ) {
+		cout << "Please enter the arithmetic expression again with the error, or enter Q/q to quit";
+		getline(cin, userInput);
+	}
+	if (userInput == "q" || userInput == "Q") {
+		cout << "Thanks for running the program!";
+		return 0;
+	}
+	//this point user input has been validated
+	
 	cout << "result is: " << validate(userInput);
+	
 
-	return 0;
+	return 1;
 }
