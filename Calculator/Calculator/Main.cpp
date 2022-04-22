a2e199691f61fbb0636c1701de6befe6c8ee1aa4
Jared Doran <doranj5121@my.uwstout.edu>
2022-04-22 15:25:23 -0500

    Added menu structure including print menu, clear, and quit

--- Calculator/Calculator/Main.cpp
+++ Calculator/Calculator/Main.cpp
@@ -4,25 +4,48 @@
 
 using namespace std;
 
+void printMenu();
+
 int main() {
 
-	string userInput;
+	string userInput = "";
 
-	cout << "Enter an arithmetic expression: ";
-	getline(cin, userInput);
+	printMenu();
 
-	while (!validate(userInput) || userInput == "Q" || userInput =="q" ) {
-		cout << "Please enter the arithmetic expression again with the error, or enter Q/q to quit";
-		getline(cin, userInput);
-	}
-	if (userInput == "q" || userInput == "Q") {
-		cout << "Thanks for running the program!";
-		return 0;
-	}
-	//this point user input has been validated
+	while (userInput != "Q" || userInput != "q") {
+		do {
+			cout << "-> ";
+			getline(cin, userInput);
+			if (userInput == "m" || userInput == "M") {
+				printMenu();
+			}
+			if (userInput == "c" || userInput == "C") {
+				system("cls");
+			}
+			if (userInput == "q" || userInput == "Q") {
+				return 0;
+			}
+		} while (userInput == "M" || userInput == "m" || userInput == "c" || userInput == "C");
+
+		bool isValid = false;
+		isValid = validate(userInput);
+
+		if (isValid) {
+			cout << "Valid" << endl;
+		}
+
+		cout << endl;
+	};
 	
-	cout << "result is: " << validate(userInput);
-	
 
-	return 1;
+	return 0;
+}
+
+void printMenu() {
+	cout << "Welcome to the Awesome Calculator Application for MSCS-271!" << endl;
+	cout << "Valid arithmatic expressions include: + - * / ^ ( )" << endl;
+	cout << "You may also perform implicit multiplication as well as use a negative (-) sign for negative numbers" << endl;
+	cout << "This means 5--5 is a valid expression" << endl;
+	cout << "At any time you can type any of the following commands -> (m) - menu, (c) - clear, (q) - quit" << endl;
+	cout << endl;
 }
