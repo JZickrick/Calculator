04215d5949e614e9777558e495131070ed784d91
Jared Doran <doranj5121@my.uwstout.edu>
2022-04-23 11:57:39 -0500

    coded in shunting yard algorithm which should be complete. Needs to be slightly modified to actually interact with token class

--- Calculator/Calculator/Evaluate.cpp
+++ Calculator/Calculator/Evaluate.cpp
@@ -1,7 +1,92 @@
-//#include "Evaluate.h"
 #include <string>
 #include <stack>
+#include <queue>
+#include <vector>
+#include <assert.h>
+
 using namespace std;
-double evaluate(string expression) {
+
+enum myType {
+	UNKNOWN = 0, NUMBER = 1, OPERATOR = 2, lPara = 3, rPara = 4
+};
+
+class token {
+public:
+	int getType() {
+		return 1;
+		//for reference of what getType will return
+		//Unknown,		//0
+		//Number,		//1
+		//Operator,		//2
+		//lPara,		//3
+		//rPara,		//4
+	}
+	string getString() {
+		return "Test String";
+	}
+	int getPrecedence() {
+		return 1;
+	}
+	bool getRightAssociative() {
+		return true;
+	}
+
+private:
+
+};
+
+double evaluate(vector<token> tokenExp) {
+
+	//vector array of tokens
+	//token.getType()
+	//token.getStr()
+
+	stack<token> tokensInOrder;
+
+	stack<token> operatorStack;
+	queue<token> outputQueue;
+
+	while (!tokenExp.empty()) {
+		token temp;
+		temp = tokenExp.back();
+		tokenExp.pop_back();
+		tokensInOrder.push(temp);
+	}
+
+	while (!tokensInOrder.empty()) {
+		token currentToken = tokensInOrder.top();
+		tokensInOrder.pop();
+		if (currentToken.getType() == NUMBER) {
+			outputQueue.push(currentToken);
+		}
+		if (currentToken.getType() == OPERATOR) {
+			//there is an operator o2 other than the left parenthesis at the top
+			//of the operator stack, and (o2 has greater precedence than o1
+			//or they have the same precedence and o1 is left - associative)
+			while (operatorStack.top().getType() != lPara && ( operatorStack.top().getPrecedence() > currentToken.getPrecedence() || ( operatorStack.top().getPrecedence() == currentToken.getPrecedence() && !currentToken.getRightAssociative()) ) ) {
+				outputQueue.push(operatorStack.top());
+				operatorStack.pop();
+			}
+			operatorStack.push(currentToken);
+		}
+		if (currentToken.getType() == lPara) {
+			operatorStack.push(currentToken);
+		}
+		if (currentToken.getType() == rPara) {
+			while (operatorStack.top().getType() != lPara) {
+				assert(!operatorStack.empty());
+				outputQueue.push(operatorStack.top());
+				operatorStack.pop();
+			}
+			assert(operatorStack.top().getType() == lPara);
+			operatorStack.pop();
+		}
+	}
+	while (!operatorStack.empty()) {
+		assert(operatorStack.top().getType() != lPara);
+		outputQueue.push(operatorStack.top());
+		operatorStack.pop();
+	}
+
 
 }
